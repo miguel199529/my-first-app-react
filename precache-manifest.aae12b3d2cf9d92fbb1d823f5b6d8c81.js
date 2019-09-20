92e00a513e5e245958ca31523c674c57d58a31f3
Miguel Ángel López <miguel@girasolo.com>
2019-09-20 17:33:26 -0500

    Updates

---
+++ precache-manifest.aae12b3d2cf9d92fbb1d823f5b6d8c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10d901094b63de6a01dea99e34b0cb0",
+    "url": "//miguel199529.github.io/my-first-app-react/index.html"
+  },
+  {
+    "revision": "4cd299441536c9d6d9bc",
+    "url": "//miguel199529.github.io/my-first-app-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "02fd1712fe809af399ee",
+    "url": "//miguel199529.github.io/my-first-app-react/static/js/2.bc9df10e.chunk.js"
+  },
+  {
+    "revision": "4cd299441536c9d6d9bc",
+    "url": "//miguel199529.github.io/my-first-app-react/static/js/main.4846bba0.chunk.js"
+  },
+  {
+    "revision": "e3c3fc722004d87fd0ad",
+    "url": "//miguel199529.github.io/my-first-app-react/static/js/runtime-main.2f2fcd82.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "//miguel199529.github.io/my-first-app-react/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "//miguel199529.github.io/my-first-app-react/precache-manifest.aae12b3d2cf9d92fbb1d823f5b6d8c81.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//miguel199529.github.io/my-first-app-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
